06aac3f0c2e13936b4f3e90867a353483d67cc57
junbetterway <jun.minon@etradingsoftware.com>
2021-11-13 17:40:36 +0800

    Provide guest and system APIs

--- src/main/java/com/junbetterway/security/springrbacauthzero/controller/AccountController.java
+++ src/main/java/com/junbetterway/security/springrbacauthzero/controller/AccountController.java
@@ -6,14 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.junbetterway.security.springrbacauthzero.model.Account;
-import com.junbetterway.security.springrbacauthzero.permission.HasCreateAccountPermission;
 import com.junbetterway.security.springrbacauthzero.permission.HasReadAccountPermission;
+import com.junbetterway.security.springrbacauthzero.permission.HasReadSystemAccountPermission;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -28,10 +26,10 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class AccountController {
 
-    @GetMapping
+    @GetMapping("guest")
     @HasReadAccountPermission
-    public ResponseEntity<Account> read() {
-    	log.info("Accessing read API - needs SCOPE_read:accounts");
+    public ResponseEntity<Account> readGuestAccount() {
+    	log.info("Accessing readGuestAccount API - needs SCOPE_read:accounts");
     	Account account = Account.builder()
     			.name("Jun King Minon")
     			.balance(new BigDecimal("5500"))
@@ -39,14 +37,14 @@ public class AccountController {
 		return new ResponseEntity<>(account, HttpStatus.OK);
     }
 
-    @PostMapping
-    @HasCreateAccountPermission
-    public ResponseEntity<Account> create(@RequestBody Account request) {
-    	log.info("Accessing create API - needs SCOPE_create:accounts!");
-    	Account account = Account.builder()
-    			.name(request.getName())
-    			.balance(request.getBalance())
-    			.build();
+    @GetMapping("system")
+    @HasReadSystemAccountPermission
+    public ResponseEntity<Account> readAdminAccount() {
+    	log.info("Accessing readAdminAccount API - needs SCOPE_read:system:accounts!");
+        Account account = Account.builder()
+                .name("Better Way System")
+                .balance(new BigDecimal("25000000"))
+                .build();
 		return new ResponseEntity<>(account, HttpStatus.OK);
     }
     
